2d9835b89d122cb29ff932f53b808c51428b0aa2
Sushma <sushmaviasblessy@gmail.com>
2025-08-05 13:05:28 +0530

    added admin and register

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { Login } from './pages/login/login';
 import { Register } from './pages/register/register';
 import { Home } from './pages/home/home';
+import { Admin } from './pages/admin/admin';
 
 export const appRoutes: Routes=[
     {path:'',component:Home},
     {path:'login',component:Login},
-    {path:'register',component:Register}
+    {path:'register',component:Register},
+    {path:'admin',component:Admin}
 ];
--- frontend/src/app/pages/admin/admin.ts
+++ frontend/src/app/pages/admin/admin.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-admin',
-  imports: [],
+  imports: [CommonModule, FormsModule],
+  standalone: true,
   templateUrl: './admin.html',
   styleUrl: './admin.css'
 })
 export class Admin {
 
 }
+
